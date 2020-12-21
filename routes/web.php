f8428432b3152b600c7b73eb6385ac46e5dcbfc4
kennCK <kennettecanales@gmail.com>
2020-12-21 11:37:17 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -296,3 +296,12 @@ Route::post($route.'retrieve', $controller."retrieve");
 Route::post($route.'validate', $controller."retrieveByValidation");
 Route::post($route.'delete', $controller."delete");
 Route::post($route.'update', $controller."update");
+
+// MyCircles Controller
+$route = env('PACKAGE_ROUTE', '').'/my_circles/';
+$controller = 'MyCircleController@';
+Route::post($route.'create', $controller."create");
+Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'validate', $controller."retrieveByValidation");
+Route::post($route.'delete', $controller."delete");
+Route::post($route.'update', $controller."update");
